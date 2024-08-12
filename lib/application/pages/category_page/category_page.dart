133794df782b0a43026961db7a6a688d93bbffac
kawser-miah <kawsermiah658@gmail.com>
2024-08-12 21:12:55 +0600

    functionable income expense banner

--- lib/application/pages/category_page/category_page.dart
+++ lib/application/pages/category_page/category_page.dart
@@ -1,60 +1,138 @@
+import 'package:finance_tracker/application/pages/category_page/bloc/category_bloc.dart';
+import 'package:finance_tracker/di/di.dart';
 import 'package:finance_tracker/utils/models/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../../generated/assets.dart';
 import '../../core/widgets/income_expense_box.dart';
+import '../home_page/bloc/home_page_bloc.dart';
 
 class CategoryPageWrapperProvider extends StatelessWidget {
   const CategoryPageWrapperProvider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          padding:
-          const EdgeInsets.only(top: 25, right: 20, left: 20, bottom: 5),
-          height: MediaQuery.of(context).size.height * 0.29,
-          width: MediaQuery.of(context).size.width,
-          color: Theme.of(context).colorScheme.primary,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                const Text("Categories",style: AppTheme.lightHeadingText,),
-                SizedBox(
-                  height: 35,
-                  width: 35,
-                  child: CircleAvatar(
-                    radius: 17.5,
-                    backgroundColor:
-                    Theme.of(context).colorScheme.primaryContainer,
-                    child: SvgPicture.asset(
-                      Assets.homeNotification,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],),
-              IncomeExpenseBox(
-                totalIncome: 50000,
-                totalExpense: 30000,
-              ),
-            ],
-          ),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.56,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(50), topRight: Radius.circular(50)),
-              color: Theme.of(context).colorScheme.onPrimary),
+    return BlocProvider(
+      create: (_) => getIt<HomePageBloc>()..add(const HomePageEvent.started()),
+      child: const CategoryPage(),
+    );
+  }
+}
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) =>
+              getIt<CategoryBloc>()..add(const CategoryRequestEvent()),
         )
       ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding:
+                const EdgeInsets.only(top: 25, right: 20, left: 20, bottom: 5),
+            height: MediaQuery.of(context).size.height * 0.29,
+            width: MediaQuery.of(context).size.width,
+            color: Theme.of(context).colorScheme.primary,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Categories",
+                      style: AppTheme.lightHeadingText,
+                    ),
+                    SizedBox(
+                      height: 35,
+                      width: 35,
+                      child: CircleAvatar(
+                        radius: 17.5,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.primaryContainer,
+                        child: SvgPicture.asset(
+                          Assets.homeNotification,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                BlocBuilder<HomePageBloc, HomePageState>(
+                  builder: (context, state) {
+                    return IncomeExpenseBox(
+                      totalIncome: state.totalBalance.totalIncome,
+                      totalExpense: state.totalBalance.totalExpense,
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(right: 20, left: 20),
+            height: MediaQuery.of(context).size.height * 0.56,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                color: Theme.of(context).colorScheme.onPrimary),
+            child: BlocBuilder<CategoryBloc, CategoryState>(
+              builder: (context, state) {
+                if (state is CategoryLoadedState) {
+                  return GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        childAspectRatio: 1,
+                        mainAxisSpacing: 30,
+                        crossAxisSpacing: 12,
+                      ),
+                      itemCount: state.categories.length,
+                      itemBuilder: (context, index) => InkWell(
+                            onTap: () {},
+                            child: Column(
+                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  height: 85,
+                                  width: 85,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(22),
+                                      // color: Theme.of(context).colorScheme.primary,
+                                      color: Colors.deepPurple.shade50),
+                                  child: SvgPicture.asset(
+                                    state.categories[index].img,
+                                    color: Colors.black,
+                                    fit: BoxFit.scaleDown,
+                                  ),
+                                ),
+                                Text(
+                                  state.categories[index].name,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                          ));
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/application/pages/home_page/home_page.dart
+++ lib/application/pages/home_page/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:finance_tracker/application/pages/home_page/bloc/home_page_bloc.dart';
+import 'package:finance_tracker/di/di.dart';
 import 'package:finance_tracker/utils/models/theme.dart';
 import 'package:finance_tracker/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import '../../../generated/assets.dart';
@@ -8,16 +11,26 @@ import '../../../utils/strings.dart';
 import '../../core/widgets/income_expense_box.dart';
 import 'tabs.dart';
 
-class HomePageWrapperProvider extends StatefulWidget {
+class HomePageWrapperProvider extends StatelessWidget {
   const HomePageWrapperProvider({super.key});
 
   @override
-  State<HomePageWrapperProvider> createState() =>
-      _HomePageWrapperProviderState();
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<HomePageBloc>()..add(const HomePageEvent.started()),
+      child: const HomePage(),
+    );
+  }
 }
 
-class _HomePageWrapperProviderState extends State<HomePageWrapperProvider>
-    with TickerProviderStateMixin {
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   late TabController _controller;
 
   @override
@@ -29,179 +42,189 @@ class _HomePageWrapperProviderState extends State<HomePageWrapperProvider>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Container(
-          padding:
-              const EdgeInsets.only(top: 25, right: 20, left: 20, bottom: 5),
-          height: MediaQuery.of(context).size.height * 0.29,
-          width: MediaQuery.of(context).size.width,
-          color: Theme.of(context).colorScheme.primary,
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return BlocBuilder<HomePageBloc, HomePageState>(
+      builder: (context, state) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 25, right: 20, left: 20, bottom: 5),
+              height: MediaQuery.of(context).size.height * 0.29,
+              width: MediaQuery.of(context).size.width,
+              color: Theme.of(context).colorScheme.primary,
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text("Hi, Welcome Back",
-                          style: AppTheme.lightHeadingText),
-                      Text(
-                        Utils.getGreeting(),
-                        style: AppTheme.lightBodyText.copyWith(fontSize: 15),
-                      )
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text("Hi, Welcome Back",
+                              style: AppTheme.lightHeadingText),
+                          Text(
+                            Utils.getGreeting(),
+                            style:
+                                AppTheme.lightBodyText.copyWith(fontSize: 15),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 35,
+                        width: 35,
+                        child: CircleAvatar(
+                          radius: 17.5,
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primaryContainer,
+                          child: SvgPicture.asset(
+                            Assets.homeNotification,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 35,
-                    width: 35,
-                    child: CircleAvatar(
-                      radius: 17.5,
-                      backgroundColor:
-                          Theme.of(context).colorScheme.primaryContainer,
-                      child: SvgPicture.asset(
-                        Assets.homeNotification,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+                  IncomeExpenseBox(
+                    totalIncome: state.totalBalance.totalIncome,
+                    totalExpense: state.totalBalance.totalExpense,
                   ),
                 ],
               ),
-              IncomeExpenseBox(
-                totalIncome: 50000,
-                totalExpense: 30000,
-              ),
-            ],
-          ),
-        ),
-        Container(
-          padding:
-              const EdgeInsets.only(top: 25, right: 20, left: 20),
-          height: MediaQuery.of(context).size.height * 0.56,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(50), topRight: Radius.circular(50)),
-              color: Theme.of(context).colorScheme.onPrimary),
-          child: Column(
-            children: [
-              Container(
-                height: 152,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(31),
-                    color: Theme.of(context).colorScheme.primary),
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CircularPercentIndicator(
-                            radius: 40,
-                            percent: 0.5,
-                            progressColor: Colors.deepPurpleAccent,
-                            backgroundColor:
-                                Theme.of(context).colorScheme.onPrimary,
-                            center: SvgPicture.asset(Assets.homeCar),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Align(
-                              alignment: Alignment.center,
-                              child: Text(
-                                "Savings\nOn Goals",
-                                style: AppTheme.lightBodyText
-                                    .copyWith(fontSize: 14),
-                              ))
-                        ],
-                      ),
-                      Container(
-                        height: 110,
-                        width: 2,
-                        color: Theme.of(context).colorScheme.onPrimary,
-                      ),
-                      Column(
+            ),
+            Container(
+              padding: const EdgeInsets.only(top: 25, right: 20, left: 20),
+              height: MediaQuery.of(context).size.height * 0.56,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)),
+                  color: Theme.of(context).colorScheme.onPrimary),
+              child: Column(
+                children: [
+                  Container(
+                    height: 152,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(31),
+                        color: Theme.of(context).colorScheme.primary),
+                    child: Center(
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          _widgetBox(context, "Revenue Last Week",
-                              Assets.homeSalary, 4000),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              CircularPercentIndicator(
+                                radius: 40,
+                                percent: 0.5,
+                                progressColor: Colors.deepPurpleAccent,
+                                backgroundColor:
+                                    Theme.of(context).colorScheme.onPrimary,
+                                center: SvgPicture.asset(Assets.homeCar),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Align(
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    "Savings\nOn Goals",
+                                    style: AppTheme.lightBodyText
+                                        .copyWith(fontSize: 14),
+                                  ))
+                            ],
+                          ),
                           Container(
-                            width: 185,
-                            height: 2,
+                            height: 110,
+                            width: 2,
                             color: Theme.of(context).colorScheme.onPrimary,
                           ),
-                          _widgetBox(
-                              context, "Food Last Week", Assets.categoryFood, 1000)
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              _widgetBox(context, "Revenue Last Month",
+                                  Assets.homeSalary, state.totalBalance.savingsLastMonth),
+                              Container(
+                                width: 185,
+                                height: 2,
+                                color: Theme.of(context).colorScheme.onPrimary,
+                              ),
+                              _widgetBox(context, "${state.totalBalance.bestExpense?.category} Last Week",
+                                  Assets.categoryFood, state.totalBalance.bestExpense?.total_expense)
+                            ],
+                          ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              DefaultTabController(
-                length: 3,
-                child: Container(
-                  margin: const EdgeInsets.only(top: 15),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(22),
-                          color: Theme.of(context).colorScheme.primaryContainer,
-                        ),
-                        child: TabBar(
-                          indicatorPadding: const EdgeInsets.all(7),
-                          indicatorSize: TabBarIndicatorSize.tab,
-                          labelColor: Colors.black,
-                          indicator: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              color: Theme.of(context).colorScheme.primary),
-                          dividerColor: Colors.transparent,
-                          tabs: const [
-                            Tab(
-                              text: "Daily",
+                  DefaultTabController(
+                    length: 3,
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 60,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(22),
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
                             ),
-                            Tab(
-                              text: "Weekly",
+                            child: TabBar(
+                              indicatorPadding: const EdgeInsets.all(7),
+                              indicatorSize: TabBarIndicatorSize.tab,
+                              labelColor: Colors.black,
+                              indicator: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Theme.of(context).colorScheme.primary),
+                              dividerColor: Colors.transparent,
+                              tabs: const [
+                                Tab(
+                                  text: "Daily",
+                                ),
+                                Tab(
+                                  text: "Weekly",
+                                ),
+                                Tab(
+                                  text: "Monthly",
+                                )
+                              ],
+                              controller: _controller,
                             ),
-                            Tab(
-                              text: "Monthly",
-                            )
-                          ],
-                          controller: _controller,
-                        ),
+                          ),
+                          SizedBox(
+                            // height: 223,
+                            height: MediaQuery.of(context).size.height * 0.2655,
+                            child:
+                                TabBarView(controller: _controller, children: [
+                              Tabs(
+                                name: Strings.daily,
+                              ),
+                              Tabs(
+                                name: Strings.weekly,
+                              ),
+                              Tabs(
+                                name: Strings.monthly,
+                              )
+                            ]),
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        // height: 223,
-                        height: MediaQuery.of(context).size.height * 0.2655,
-                        child: TabBarView(controller: _controller, children: [
-                          Tabs(
-                            name: Strings.daily,
-                          ),
-                          Tabs(
-                            name: Strings.weekly,
-                          ),
-                          Tabs(name: Strings.monthly,)
-                        ]),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        )
-      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 
   Widget _widgetBox(
-      BuildContext context, String msg, String image, double amount) {
+      BuildContext context, String msg, String image, double? amount) {
     return Row(
       children: [
         SvgPicture.asset(
