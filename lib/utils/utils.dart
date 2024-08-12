502ee8958282a6027b98c2145f608a8ad222672d
kawser-miah <kawsermiah658@gmail.com>
2024-08-12 21:10:51 +0600

    add category list

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,8 @@
 import 'package:finance_tracker/utils/strings.dart';
 
 import '../application/core/category_enum.dart';
+import '../domain/models/income_expense_category.dart';
+import '../generated/assets.dart';
 
 class Utils {
   static String getGreeting() {
@@ -27,4 +29,16 @@ class Utils {
         return Strings.todayErrorMsg;
     }
   }
+
+  static final List<Category> categoriesList=[
+    const Category(id: 1, img: Assets.categoryFood, name: 'Food'),
+    const Category(id: 2, img: Assets.categoryTransport, name: 'Transport'),
+    const Category(id: 3, img: Assets.categoryGroceries, name: 'Groceries'),
+    const Category(id: 4, img: Assets.categoryMedicine, name: 'Medicine'),
+    const Category(id: 5, img: Assets.categoryRent, name: 'Rent'),
+    const Category(id: 6, img: Assets.categoryGifts, name: 'Gifts'),
+    const Category(id: 7, img: Assets.categoryEntertainment, name: 'Entertainment'),
+    const Category(id: 8, img: Assets.homeSalary, name: 'Savings'),
+    const Category(id: 9, img: Assets.categoryMore, name: 'Add')
+  ];
 }
