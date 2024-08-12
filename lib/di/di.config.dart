be0df98fc91a8d79313fcaec663e552dba3c76cf
kawser-miah <kawsermiah658@gmail.com>
2024-08-12 21:17:17 +0600

    add home page bloc

--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -9,46 +9,72 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:finance_tracker/application/pages/bottom_nav/bloc/bottom_bloc.dart'
-    as _i3;
+    as _i854;
+import 'package:finance_tracker/application/pages/category_page/bloc/category_bloc.dart'
+    as _i782;
+import 'package:finance_tracker/application/pages/home_page/bloc/home_page_bloc.dart'
+    as _i54;
 import 'package:finance_tracker/application/pages/transaction_page/bloc/transaction_bloc.dart'
-    as _i8;
+    as _i709;
 import 'package:finance_tracker/data/datasources/db/finance_tracker_db.dart'
-    as _i4;
+    as _i273;
+import 'package:finance_tracker/data/repositories/category_repository_iml.dart'
+    as _i25;
+import 'package:finance_tracker/data/repositories/home_page_repository_iml.dart'
+    as _i256;
 import 'package:finance_tracker/data/repositories/transaction_repository_iml.dart'
-    as _i6;
-import 'package:finance_tracker/di/register_module.dart' as _i9;
+    as _i338;
+import 'package:finance_tracker/di/register_module.dart' as _i827;
+import 'package:finance_tracker/domain/repositories/category_repository.dart'
+    as _i192;
+import 'package:finance_tracker/domain/repositories/home_page_repository.dart'
+    as _i856;
 import 'package:finance_tracker/domain/repositories/transaction_repository.dart'
-    as _i5;
+    as _i163;
+import 'package:finance_tracker/domain/usecases/category_usecase.dart' as _i347;
+import 'package:finance_tracker/domain/usecases/home_page_usecase.dart' as _i89;
 import 'package:finance_tracker/domain/usecases/transaction_usecase.dart'
-    as _i7;
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
+    as _i815;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
 
-extension GetItInjectableX on _i1.GetIt {
+extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  Future<_i1.GetIt> init({
+  Future<_i174.GetIt> init({
     String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
+    _i526.EnvironmentFilter? environmentFilter,
   }) async {
-    final gh = _i2.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
     final registerModule = _$RegisterModule();
-    gh.factory<_i3.BottomBloc>(() => _i3.BottomBloc());
-    await gh.factoryAsync<_i4.FinanceTrackerDB>(
+    gh.factory<_i854.BottomBloc>(() => _i854.BottomBloc());
+    await gh.factoryAsync<_i273.FinanceTrackerDB>(
       () => registerModule.db,
       preResolve: true,
     );
-    gh.lazySingleton<_i5.TransactionRepository>(
-        () => _i6.TransactionRepositoryIml(db: gh<_i4.FinanceTrackerDB>()));
-    gh.factory<_i7.TransactionUseCase>(() => _i7.TransactionUseCase(
-        transactionRepository: gh<_i5.TransactionRepository>()));
-    gh.factory<_i8.TransactionBloc>(() =>
-        _i8.TransactionBloc(transactionUseCase: gh<_i7.TransactionUseCase>()));
+    gh.lazySingleton<_i192.CategoryRepository>(
+        () => _i25.CategoryRepositoryIml());
+    gh.lazySingleton<_i856.HomePageRepository>(
+        () => _i256.HomePageRepositoryIml(db: gh<_i273.FinanceTrackerDB>()));
+    gh.lazySingleton<_i163.TransactionRepository>(
+        () => _i338.TransactionRepositoryIml(db: gh<_i273.FinanceTrackerDB>()));
+    gh.factory<_i89.HomePageUseCase>(() => _i89.HomePageUseCase(
+        homePageRepository: gh<_i856.HomePageRepository>()));
+    gh.factory<_i347.CategoryUseCase>(() => _i347.CategoryUseCase(
+        categoryRepository: gh<_i192.CategoryRepository>()));
+    gh.factory<_i782.CategoryBloc>(
+        () => _i782.CategoryBloc(categoryUseCase: gh<_i347.CategoryUseCase>()));
+    gh.factory<_i815.TransactionUseCase>(() => _i815.TransactionUseCase(
+        transactionRepository: gh<_i163.TransactionRepository>()));
+    gh.factory<_i54.HomePageBloc>(
+        () => _i54.HomePageBloc(homePageUseCase: gh<_i89.HomePageUseCase>()));
+    gh.factory<_i709.TransactionBloc>(() => _i709.TransactionBloc(
+        transactionUseCase: gh<_i815.TransactionUseCase>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i9.RegisterModule {}
+class _$RegisterModule extends _i827.RegisterModule {}
