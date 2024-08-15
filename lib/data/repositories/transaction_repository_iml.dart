8a4e666ab0036d78c5cba6187cf8b0067c940843
kawser-miah <kawsermiah658@gmail.com>
2024-08-15 18:14:06 +0600

    removed unused code

--- lib/data/repositories/transaction_repository_iml.dart
+++ lib/data/repositories/transaction_repository_iml.dart
@@ -15,83 +15,139 @@ class TransactionRepositoryIml extends TransactionRepository {
   @override
   Future<Either<Failure, List<TransactionModel>>>
       getTransactionDataByCurrentDate() async {
-
     try {
       List<TransactionModel> transactions = [];
       final incomeResult = await db.incomeDao.getIncomeDataByCurrentDate();
       final expenseResult = await db.expenseDao.getExpenseDataByCurrentDate();
 
-      if(incomeResult.isNotEmpty || expenseResult.isNotEmpty){
-        for(var i in incomeResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.income, description: i.description??"You do not add any descriptions...!", date: i.date));
+      if (incomeResult.isNotEmpty || expenseResult.isNotEmpty) {
+        for (var i in incomeResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.income,
+              description: i.description,
+              date: i.date,
+              title: i.incomeTitle));
         }
-        for(var i in expenseResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.expense, description: i.description??"You do not add any descriptions...!", date: i.date));
+        for (var i in expenseResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.expense,
+              description: i.description,
+              date: i.date,
+              title: i.expenseTitle));
         }
-      }else{
+      } else {
         final incomeResult = await db.incomeDao.getIncomeDataByPreviousDate();
-        final expenseResult = await db.expenseDao.getExpenseDataByPreviousDate();
-        if(incomeResult.isNotEmpty || expenseResult.isNotEmpty){
-          for(var i in incomeResult){
-            transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.income, description: i.description??"You do not add any descriptions...!", date: i.date));
+        final expenseResult =
+            await db.expenseDao.getExpenseDataByPreviousDate();
+        if (incomeResult.isNotEmpty || expenseResult.isNotEmpty) {
+          for (var i in incomeResult) {
+            transactions.add(TransactionModel(
+                id: i.id,
+                category: i.category,
+                type: i.type,
+                amount: i.income,
+                description: i.description,
+                date: i.date,
+                title: i.incomeTitle));
           }
-          for(var i in expenseResult){
-            transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.expense, description: i.description??"You do not add any descriptions...!", date: i.date));
+          for (var i in expenseResult) {
+            transactions.add(TransactionModel(
+                id: i.id,
+                category: i.category,
+                type: i.type,
+                amount: i.expense,
+                description: i.description,
+                date: i.date,
+                title: i.expenseTitle));
           }
-        }else{
+        } else {
           return left(NullFailure());
         }
       }
       return right(transactions);
-    } catch(e) {
+    } catch (e) {
       throw ServerException();
     }
   }
 
   @override
   Future<Either<Failure, List<TransactionModel>>>
-      getTransactionDataByCurrentMonth() async{
-    try{
+      getTransactionDataByCurrentMonth() async {
+    try {
       List<TransactionModel> transactions = [];
       final incomeResult = await db.incomeDao.getIncomeDataByCurrentMonth();
       final expenseResult = await db.expenseDao.getExpenseDataByCurrentMonth();
 
-      if(incomeResult.isNotEmpty || expenseResult.isNotEmpty){
-        for(var i in incomeResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.income, description: i.description??"You do not add any descriptions...!", date: i.date));
+      if (incomeResult.isNotEmpty || expenseResult.isNotEmpty) {
+        for (var i in incomeResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.income,
+              description: i.description,
+              date: i.date,
+              title: i.incomeTitle));
         }
-        for(var i in expenseResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.expense, description: i.description??"You do not add any descriptions...!", date: i.date));
+        for (var i in expenseResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.expense,
+              description: i.description,
+              date: i.date,
+              title: i.expenseTitle));
         }
-      }else{
+      } else {
         return left(NullFailure());
       }
       return right(transactions);
-    }catch(e){
+    } catch (e) {
       throw ServerException();
     }
   }
 
   @override
   Future<Either<Failure, List<TransactionModel>>>
-      getTransactionDataByCurrentWeek() async{
-    try{
+      getTransactionDataByCurrentWeek() async {
+    try {
       List<TransactionModel> transactions = [];
       final incomeResult = await db.incomeDao.getIncomeDataByCurrentWeek();
       final expenseResult = await db.expenseDao.getExpenseDataByCurrentWeek();
 
-      if(incomeResult.isNotEmpty || expenseResult.isNotEmpty){
-        for(var i in incomeResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.income, description: i.description??"You do not add any descriptions...!", date: i.date));
+      if (incomeResult.isNotEmpty || expenseResult.isNotEmpty) {
+        for (var i in incomeResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.income,
+              description: i.description,
+              date: i.date,
+              title: i.incomeTitle));
         }
-        for(var i in expenseResult){
-          transactions.add(TransactionModel(id: i.id, category: i.category, type: i.type, amount: i.expense, description: i.description ??"You do not add any descriptions...!", date: i.date));
+        for (var i in expenseResult) {
+          transactions.add(TransactionModel(
+              id: i.id,
+              category: i.category,
+              type: i.type,
+              amount: i.expense,
+              description: i.description,
+              date: i.date,
+              title: i.expenseTitle));
         }
-      }else{
+      } else {
         return left(NullFailure());
       }
       return right(transactions);
-    }catch(e){
+    } catch (e) {
       throw ServerException();
     }
   }
