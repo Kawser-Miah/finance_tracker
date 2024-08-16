3c65bd16b74d77c2470ba78b8390d029bf2def6f
kawser-miah <kawsermiah658@gmail.com>
2024-08-16 21:30:56 +0600

    add some strings

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -6,5 +6,7 @@ class Strings{
   load = "Loading...",
   todayErrorMsg ="Today you do not added any income or expense..!",
   weekErrorMsg ="You do not added any income or expense in this week..!",
-  monthErrorMsg="You do not added any income or expense in this Month..!";
+  monthErrorMsg="You do not added any income or expense in this Month..!",
+  income = 'Income',
+  expense = 'Expense';
 }
