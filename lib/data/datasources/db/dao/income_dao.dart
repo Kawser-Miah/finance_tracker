ef45206dfdbdb370f6e8adaf98ede554052a7c9d
kawser-miah <kawsermiah658@gmail.com>
2024-08-18 00:23:51 +0600

    update all query for local time

--- lib/data/datasources/db/dao/income_dao.dart
+++ lib/data/datasources/db/dao/income_dao.dart
@@ -4,22 +4,22 @@ import 'package:floor/floor.dart';
 @dao
 abstract class IncomeDao {
   
-  @Query("SELECT * FROM incomes WHERE DATE(date) = DATE('now') ORDER BY date DESC")
+  @Query("SELECT * FROM incomes WHERE DATE(date) = DATE('now','localtime') ORDER BY date DESC")
   Future<List<IncomeEntityModel>> getIncomeDataByCurrentDate();
 
-  @Query("SELECT * FROM incomes WHERE DATE(date) = DATE('now','-1 day') ORDER BY date DESC")
+  @Query("SELECT * FROM incomes WHERE DATE(date) = DATE('now','-1 day','localtime') ORDER BY date DESC")
   Future<List<IncomeEntityModel>> getIncomeDataByPreviousDate();
 
-  @Query("SELECT * FROM incomes WHERE DATE(date) BETWEEN DATE('now', 'weekday 0', '-7 days') AND DATE('now', 'weekday 0') ORDER BY date DESC;")
+  @Query("SELECT * FROM incomes WHERE DATE(date) BETWEEN DATE('now', 'weekday 0', '-7 days','localtime') AND DATE('now', 'weekday 0','localtime') ORDER BY date DESC;")
   Future<List<IncomeEntityModel>> getIncomeDataByCurrentWeek();
   
-  @Query("SELECT * FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now') ORDER BY date DESC;")
+  @Query("SELECT * FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now','localtime') ORDER BY date DESC;")
   Future<List<IncomeEntityModel>> getIncomeDataByCurrentMonth();
   
-  @Query("SELECT SUM(income) AS total_income FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now')")
+  @Query("SELECT SUM(income) AS total_income FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now','localtime')")
   Future<double?> getTotalIncomeByCurrentMonth();
 
-  @Query("SELECT SUM(income) AS total_income FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now','-1 month')")
+  @Query("SELECT SUM(income) AS total_income FROM incomes WHERE strftime('%Y-%m', date) = strftime('%Y-%m', 'now','-1 month','localtime')")
   Future<double?> getTotalIncomeByPreviousMonth();
 
   @Query("DELETE FROM incomes WHERE id =:id")
