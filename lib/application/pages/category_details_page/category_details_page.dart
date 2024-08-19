34491d005f0dd328fe181e4f5927c0b4877d4e68
kawser-miah <kawsermiah658@gmail.com>
2024-08-19 19:45:31 +0600

    change the view

--- lib/application/pages/category_details_page/category_details_page.dart
+++ lib/application/pages/category_details_page/category_details_page.dart
@@ -1,15 +1,18 @@
 import 'package:finance_tracker/application/core/services/routing/app_router.dart';
-import 'package:finance_tracker/application/pages/category_page/bloc/category_bloc.dart';
+import 'package:finance_tracker/application/pages/category_details_page/bloc/category_details_bloc.dart';
+import 'package:finance_tracker/application/pages/data_insert_page/bloc/income_expense_add_bloc.dart';
 import 'package:finance_tracker/di/di.dart';
 import 'package:finance_tracker/domain/models/transaction_model.dart';
 import 'package:finance_tracker/utils/models/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
 import '../../../utils/strings.dart';
 import '../../core/services/routing/route_utils.dart';
+import '../../core/widgets/category_details_container.dart';
+import '../../core/widgets/exception_container.dart';
 import '../../core/widgets/income_expense_box.dart';
+
 import '../home_page/bloc/home_page_bloc.dart';
 
 class CategoryDetailsPage extends StatelessWidget {
@@ -29,9 +32,10 @@ class CategoryDetailsPage extends StatelessWidget {
               getIt<HomePageBloc>()..add(const HomePageEvent.started()),
         ),
         BlocProvider(
-          create: (_) =>
-              getIt<CategoryBloc>()..add(const CategoryRequestEvent()),
-        )
+          create: (_) => getIt<CategoryDetailsBloc>()
+            ..add(CategoryDetailsEvent.started(category: category)),
+        ),
+        BlocProvider(create: (_)=>getIt<IncomeExpenseAddBloc>())
       ],
       child: CustomCategoryDetailsPage(
         category: category,
@@ -108,45 +112,43 @@ class CustomCategoryDetailsPage extends StatelessWidget {
               children: [
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.6,
-                  child: BlocBuilder<CategoryBloc, CategoryState>(
+                  child: BlocBuilder<CategoryDetailsBloc, CategoryDetailsState>(
                     builder: (context, state) {
-                      if (state is CategoryLoadedState) {
-                        return GridView.builder(
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 3,
-                              childAspectRatio: 1,
-                              mainAxisSpacing: 30,
-                              crossAxisSpacing: 12,
-                            ),
-                            itemCount: state.categories.length,
-                            itemBuilder: (context, index) => InkWell(
-                                  onTap: () {},
-                                  child: Column(
-                                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Container(
-                                        height: 85,
-                                        width: 85,
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(22),
-                                            // color: Theme.of(context).colorScheme.primary,
-                                            color: Colors.deepPurple.shade50),
-                                        child: SvgPicture.asset(
-                                          state.categories[index].img,
-                                          color: Colors.black,
-                                          fit: BoxFit.scaleDown,
-                                        ),
-                                      ),
-                                      Text(
-                                        state.categories[index].name,
-                                        style: const TextStyle(
+                      if (state is CategoryDetailsLoadedState) {
+                        DateFormat dateFormat = DateFormat('MMMM yyyy');
+                        return ListView.builder(
+                            itemCount: state.months.length,
+                            itemBuilder: (context, index) => Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        dateFormat.format(DateFormat("MM-yyyy")
+                                            .parse(state.months[index])),
+                                        style: AppTheme.lightBodyText.copyWith(
                                             fontWeight: FontWeight.w500),
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                    Column(
+                                      children: state.transactions
+                                          .where((transaction) =>
+                                              dateFormat.format(DateTime.parse(
+                                                  transaction.date
+                                                      .toString())) ==
+                                              dateFormat.format(
+                                                  DateFormat("MM-yyyy").parse(
+                                                      state.months[index])))
+                                          .map((transaction) =>
+                                              CategoryDetailsContainer(
+                                                transaction: transaction,
+                                              ))
+                                          .toList(),
+                                    )
+                                  ],
                                 ));
+                      } else if (state is CategoryDetailsErrorState) {
+                        return ErrorMessage(message: state.errorMessage);
                       }
                       return const Center(
                         child: CircularProgressIndicator(),
@@ -176,11 +178,6 @@ class CustomCategoryDetailsPage extends StatelessWidget {
                                     .format(DateTime.now())),
                             PAGES.categoryDetails.screenPath
                           ]);
-                          //     .then((_) {
-                          //   context
-                          //       .read<HomePageBloc>()
-                          //       .add(const HomePageEvent.started());
-                          // });
                         },
                         child: Container(
                           margin: const EdgeInsets.only(top: 2, bottom: 2),
@@ -210,11 +207,6 @@ class CustomCategoryDetailsPage extends StatelessWidget {
                                     .format(DateTime.now())),
                             PAGES.categoryDetails.screenPath
                           ]);
-                          //     .then((_) {
-                          //   context
-                          //       .read<HomePageBloc>()
-                          //       .add(const HomePageEvent.started());
-                          // });
                         },
                         child: Container(
                           margin: const EdgeInsets.only(top: 2, bottom: 2),
