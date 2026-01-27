d696fe469aab34b1cc034e9025d57c7f927e7837
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-28 01:38:26 +0600

    Refactor: Update database schema to include analysis views and DAO
    - Adds `TimeSeriesTotal` view to the database configuration.
    - Integrates `AnalysisDao` into the `FinanceTrackerDB` abstract class.
    - Updates database entity and view registration for floor generation.

--- lib/data/datasources/db/finance_tracker_db.dart
+++ lib/data/datasources/db/finance_tracker_db.dart
@@ -9,11 +9,19 @@ import 'package:floor/floor.dart';
 import 'package:injectable/injectable.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 
+import '../../../domain/models/entity_model/time_series_total_model.dart';
+import 'dao/analysis_dao.dart';
+
 part 'finance_tracker_db.g.dart';
 
-@Database(version: 1, entities: [IncomeEntityModel, ExpenseEntityModel, BestExpense])
+@Database(
+  version: 1,
+  entities: [IncomeEntityModel, ExpenseEntityModel, BestExpense],
+    views: [TimeSeriesTotal],
+)
 abstract class FinanceTrackerDB extends FloorDatabase {
   @injectable
   IncomeDao get incomeDao;
   ExpenseDao get expenseDao;
+  AnalysisDao get analysisDao;
 }
