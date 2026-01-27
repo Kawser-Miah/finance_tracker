42ace8c1be2ac4f8bb5976273b31c651cbb8cc6d
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-28 01:39:05 +0600

    Feat: Add TimeSeriesTotal database view model
    Introduces the `TimeSeriesTotal` class and corresponding `@DatabaseView` to represent aggregated income and expense data over a specific period.

---
+++ lib/domain/models/entity_model/time_series_total_model.dart
@@ -0,0 +1,15 @@
+import 'package:floor/floor.dart';
+
+@DatabaseView('''
+SELECT 
+  '' AS period,
+  0.0 AS income,
+  0.0 AS expense
+''')
+class TimeSeriesTotal {
+  final String period;
+  final double income;
+  final double expense;
+
+  TimeSeriesTotal(this.period, this.income, this.expense);
+}
