7e4a94d94f5c90ee2637a9b5c7a663fa1682d49f
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-28 01:38:50 +0600

    build: Update generated Freezed code for `IncomeExpenseAddEvent`
    The changes reflect a version update in the Freezed code generator:
    - Replaced `@JsonKey(includeFromJson: false, includeToJson: false)` with the more concise `@JsonKey(ignore: true)`.
    - Removed redundant "Create a copy of..." comments from generated `copyWith` methods.

--- lib/domain/models/income_expense_category.freezed.dart
+++ lib/domain/models/income_expense_category.freezed.dart
@@ -20,9 +20,7 @@ mixin _$Category {
   String get img => throw _privateConstructorUsedError;
   String get name => throw _privateConstructorUsedError;
 
-  /// Create a copy of Category
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   $CategoryCopyWith<Category> get copyWith =>
       throw _privateConstructorUsedError;
 }
@@ -45,8 +43,6 @@ class _$CategoryCopyWithImpl<$Res, $Val extends Category>
   // ignore: unused_field
   final $Res Function($Val) _then;
 
-  /// Create a copy of Category
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
@@ -90,8 +86,6 @@ class __$$CategoryImplCopyWithImpl<$Res>
       _$CategoryImpl _value, $Res Function(_$CategoryImpl) _then)
       : super(_value, _then);
 
-  /// Create a copy of Category
-  /// with the given fields replaced by the non-null parameter values.
   @pragma('vm:prefer-inline')
   @override
   $Res call({
@@ -147,9 +141,7 @@ class _$CategoryImpl implements _Category {
   @override
   int get hashCode => Object.hash(runtimeType, id, img, name);
 
-  /// Create a copy of Category
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   @override
   @pragma('vm:prefer-inline')
   _$$CategoryImplCopyWith<_$CategoryImpl> get copyWith =>
@@ -168,11 +160,8 @@ abstract class _Category implements Category {
   String get img;
   @override
   String get name;
-
-  /// Create a copy of Category
-  /// with the given fields replaced by the non-null parameter values.
   @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
+  @JsonKey(ignore: true)
   _$$CategoryImplCopyWith<_$CategoryImpl> get copyWith =>
       throw _privateConstructorUsedError;
 }
