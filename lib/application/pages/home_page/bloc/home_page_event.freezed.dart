7f0f97fd785f63ed1f15f3242942ba7528e07f4d
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-28 01:38:35 +0600

    Refactor: Remove redundant documentation comments in generated HomePageEvent code
    Updates `home_page_event.freezed.dart` to remove unnecessary "Create a copy of HomePageEvent" comments from the generated CopyWith classes.

--- lib/application/pages/home_page/bloc/home_page_event.freezed.dart
+++ lib/application/pages/home_page/bloc/home_page_event.freezed.dart
@@ -66,9 +66,6 @@ class _$HomePageEventCopyWithImpl<$Res, $Val extends HomePageEvent>
   final $Val _value;
   // ignore: unused_field
   final $Res Function($Val) _then;
-
-  /// Create a copy of HomePageEvent
-  /// with the given fields replaced by the non-null parameter values.
 }
 
 /// @nodoc
@@ -85,9 +82,6 @@ class __$$StartedImplCopyWithImpl<$Res>
   __$$StartedImplCopyWithImpl(
       _$StartedImpl _value, $Res Function(_$StartedImpl) _then)
       : super(_value, _then);
-
-  /// Create a copy of HomePageEvent
-  /// with the given fields replaced by the non-null parameter values.
 }
 
 /// @nodoc
