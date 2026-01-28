fbc38175e3ae5bb062952b2c493287197a393de7
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-29 01:13:09 +0600

    Refactor: Update macOS plugin registrations
    The `GeneratedPluginRegistrant.swift` for macOS was updated to replace the `sqflite` and `path_provider_foundation` imports with `sqflite_darwin`. Additionally, the registration for `PathProviderPlugin` was removed.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import flutter_local_notifications
-import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
