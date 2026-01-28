041a2dd3c4936584e13a97266d356b7d75bdf376
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-28 19:33:03 +0600

    Refactor: Update `AnalysisUseCase` to determine analysis type via `CategoryEnum`
    The `call`, `callMonthly`, and `callYearly` methods in `AnalysisUseCase` have been consolidated into a single `call` method. This new implementation accepts a string parameter, converts it to a `CategoryEnum`, and uses a switch statement to delegate the request to the appropriate repository method (daily, monthly, or yearly).

--- lib/domain/usecases/analysis_use_case.dart
+++ lib/domain/usecases/analysis_use_case.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 
+import '../../application/core/category_enum.dart';
 import '../failures/failures.dart';
 import '../models/entity_model/time_series_total_model.dart';
 import '../repositories/analysis_repository.dart';
@@ -10,10 +11,17 @@ class AnalysisUseCase {
   final AnalysisRepository analysisRepository;
 
   AnalysisUseCase({required this.analysisRepository});
-  Future<Either<Failure, List<TimeSeriesTotal>>> call() async =>
-      await analysisRepository.getDailyAnalysis();
-  Future<Either<Failure, List<TimeSeriesTotal>>> callMonthly() async =>
-      await analysisRepository.getMonthlyAnalysis();
-  Future<Either<Failure, List<TimeSeriesTotal>>> callYearly() async =>
-      await analysisRepository.getYearlyAnalysis();
+  Future<Either<Failure, List<TimeSeriesTotal>>> call(String name) async {
+    CategoryEnum categoryEnum = CategoryEnum.getEnum(name);
+    switch (categoryEnum) {
+      case CategoryEnum.daily:
+        return await analysisRepository.getDailyAnalysis();
+      case CategoryEnum.monthly:
+        return await analysisRepository.getMonthlyAnalysis();
+      case CategoryEnum.yearly:
+        return await analysisRepository.getYearlyAnalysis();
+      default:
+        return await analysisRepository.getDailyAnalysis();
+    }
+  }
 }
