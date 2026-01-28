fbe7572a51f639b4ecdf41184717939b693aedae
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-29 01:12:11 +0600

    Feat: Implement AnalysisBloc for data processing
    The `AnalysisBloc` is introduced to handle the state management for the analysis page. It utilizes `AnalysisUseCase` to fetch time series data, managing transitions between initial, loading, data loaded, and error states.

---
+++ lib/application/pages/analysis_page/bloc/analysis_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../../domain/models/entity_model/time_series_total_model.dart';
+import '../../../../domain/usecases/analysis_use_case.dart';
+
+part 'analysis_event.dart';
+part 'analysis_state.dart';
+part 'analysis_bloc.freezed.dart';
+
+@injectable
+class AnalysisBloc extends Bloc<AnalysisEvent, AnalysisState> {
+  final AnalysisUseCase analysisUseCase;
+  AnalysisBloc(this.analysisUseCase) : super(const AnalysisState.initial()) {
+    on<AnalysisEvent>((event, emit) async {
+      emit(const AnalysisState.loading());
+      try {
+        final result = await analysisUseCase.call(event.name);
+        result.fold(
+          (l) => emit(
+            const AnalysisState.error(errorMessage: "Data Not Found...!"),
+          ),
+          (r) => emit(AnalysisState.dataLoaded(data: r)),
+        );
+      } catch (e) {
+        emit(const AnalysisState.error(errorMessage: "Something went wrong"));
+      }
+    });
+  }
+}
