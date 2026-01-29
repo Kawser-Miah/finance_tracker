f32f41b0827d832d35f700900bcbab19ebb2ff40
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-29 21:27:21 +0600

    Add `share_plus` dependency and register macOS plugin
    The `share_plus` package was added to `pubspec.yaml` to enable content sharing functionality. Additionally, the plugin was registered in the macOS `GeneratedPluginRegistrant.swift` to support the platform-specific implementation.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_local_notifications
+import share_plus
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
