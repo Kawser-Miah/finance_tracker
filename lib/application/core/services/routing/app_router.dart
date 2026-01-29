529b969c7dcd872ad74694c8cf169192f0987297
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-29 19:01:50 +0600

    UI: Add tooltips to bottom navigation bar items
    Updated the `BottomNavigationBar` in `bottom_nav_page.dart` to include specific `tooltip` values for the Home, Analytics, Transaction, Category, and Profile items, improving accessibility and user guidance.

--- lib/application/core/services/routing/app_router.dart
+++ lib/application/core/services/routing/app_router.dart
@@ -6,42 +6,60 @@ import 'package:go_router/go_router.dart';
 import '../../../pages/bottom_nav/bottom_nav_page.dart';
 import '../../../pages/category_details_page/category_details_page.dart';
 import '../../../pages/data_insert_page/data_insert_page.dart';
+import '../../../pages/settings_page/privacy_policy_page.dart';
+import '../../../pages/settings_page/terms_of_service_page.dart';
 import 'not_found_screen.dart';
 
 class AppRouter {
   static final rootNavigatorKey = GlobalKey<NavigatorState>();
 
   static final GoRouter _router = GoRouter(
-      debugLogDiagnostics: true,
-      navigatorKey: rootNavigatorKey,
-      routes: [
-        GoRoute(
-            path: PAGES.bottom.screenPath,
-            name: PAGES.bottom.screenName,
-            builder: (BuildContext context, state) => const BottomNavPage()),
-        GoRoute(
-            path: PAGES.categoryDetails.screenPath,
-            name: PAGES.categoryDetails.screenName,
-            builder: (context, state) {
-              String category = state.extra as String;
+    debugLogDiagnostics: true,
+    navigatorKey: rootNavigatorKey,
+    routes: [
+      GoRoute(
+        path: PAGES.bottom.screenPath,
+        name: PAGES.bottom.screenName,
+        builder: (BuildContext context, state) => const BottomNavPage(),
+      ),
+      GoRoute(
+        path: PAGES.categoryDetails.screenPath,
+        name: PAGES.categoryDetails.screenName,
+        builder: (context, state) {
+          String category = state.extra as String;
 
-              return CategoryDetailsPage(
-                category: category,
-              );
-            }),
-        GoRoute(
-            path: PAGES.insert.screenPath,
-            name: PAGES.insert.screenName,
-            builder: (context, state) {
-              final List<dynamic> extras = state.extra as List<dynamic>;
+          return CategoryDetailsPage(category: category);
+        },
+      ),
+      GoRoute(
+        path: PAGES.insert.screenPath,
+        name: PAGES.insert.screenName,
+        builder: (context, state) {
+          final List<dynamic> extras = state.extra as List<dynamic>;
 
-              String todo = extras[0] as String;
-              TransactionModel transaction = extras[1] as TransactionModel;
-              String path = extras[2] as String;
-              return DataInsertPage(todo: todo, transaction: transaction, path: path,);
-            })
-      ],
-      errorBuilder: (context, state) => const NotFoundScreen());
+          String todo = extras[0] as String;
+          TransactionModel transaction = extras[1] as TransactionModel;
+          String path = extras[2] as String;
+          return DataInsertPage(
+            todo: todo,
+            transaction: transaction,
+            path: path,
+          );
+        },
+      ),
+      GoRoute(
+        path: PAGES.privacyPolicy.screenPath,
+        name: PAGES.privacyPolicy.screenName,
+        builder: (context, state) => const PrivacyPolicyPage(),
+      ),
+      GoRoute(
+        path: PAGES.termsOfService.screenPath,
+        name: PAGES.termsOfService.screenName,
+        builder: (context, state) => const TermsOfServicePage(),
+      ),
+    ],
+    errorBuilder: (context, state) => const NotFoundScreen(),
+  );
 
   static GoRouter get router => _router;
 }
