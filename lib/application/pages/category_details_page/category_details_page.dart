73ef5fd4070aa8c6d13484d223f0d6d1e067ebb6
Kawser Miah <138445662+Kawser-Miah@users.noreply.github.com>
2026-01-29 17:06:52 +0600

    Refactor: Remove unused icon row in `income_expense_chart_card_widget.dart`
    The `Row` containing placeholder icons in the `IncomeExpenseChartCard` header has been commented out to clean up the UI.

--- lib/application/pages/category_details_page/category_details_page.dart
+++ lib/application/pages/category_details_page/category_details_page.dart
@@ -18,10 +18,7 @@ import '../home_page/bloc/home_page_bloc.dart';
 class CategoryDetailsPage extends StatelessWidget {
   final String category;
 
-  const CategoryDetailsPage({
-    super.key,
-    required this.category,
-  });
+  const CategoryDetailsPage({super.key, required this.category});
 
   @override
   Widget build(BuildContext context) {
@@ -32,24 +29,20 @@ class CategoryDetailsPage extends StatelessWidget {
               getIt<HomePageBloc>()..add(const HomePageEvent.started()),
         ),
         BlocProvider(
-          create: (_) => getIt<CategoryDetailsBloc>()
-            ..add(CategoryDetailsEvent.started(category: category)),
+          create: (_) =>
+              getIt<CategoryDetailsBloc>()
+                ..add(CategoryDetailsEvent.started(category: category)),
         ),
-        BlocProvider(create: (_)=>getIt<IncomeExpenseAddBloc>())
+        BlocProvider(create: (_) => getIt<IncomeExpenseAddBloc>()),
       ],
-      child: CustomCategoryDetailsPage(
-        category: category,
-      ),
+      child: CustomCategoryDetailsPage(category: category),
     );
   }
 }
 
 class CustomCategoryDetailsPage extends StatelessWidget {
   final String category;
-  const CustomCategoryDetailsPage({
-    super.key,
-    required this.category,
-  });
+  const CustomCategoryDetailsPage({super.key, required this.category});
 
   @override
   Widget build(BuildContext context) {
@@ -59,8 +52,12 @@ class CustomCategoryDetailsPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            padding:
-                const EdgeInsets.only(top: 45, right: 16, left: 16, bottom: 5),
+            padding: const EdgeInsets.only(
+              top: 45,
+              right: 16,
+              left: 16,
+              bottom: 5,
+            ),
             height: MediaQuery.of(context).size.height * 0.31,
             width: MediaQuery.of(context).size.width,
             color: Theme.of(context).colorScheme.primary,
@@ -70,21 +67,17 @@ class CustomCategoryDetailsPage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     IconButton(
-                        // iconSize: 20,
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back_ios_new,
-                          // size: 20,
-                        )),
-                    const SizedBox(
-                      width: 12,
+                      // iconSize: 20,
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new,
+                        // size: 20,
+                      ),
                     ),
-                    Text(
-                      category,
-                      style: AppTheme.lightHeadingText,
-                    ),
+                    const SizedBox(width: 12),
+                    Text(category, style: AppTheme.lightHeadingText),
                   ],
                 ),
                 BlocBuilder<HomePageBloc, HomePageState>(
@@ -103,10 +96,12 @@ class CustomCategoryDetailsPage extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.69,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50)),
-                color: Theme.of(context).colorScheme.onPrimary),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -117,42 +112,52 @@ class CustomCategoryDetailsPage extends StatelessWidget {
                       if (state is CategoryDetailsLoadedState) {
                         DateFormat dateFormat = DateFormat('MMMM yyyy');
                         return ListView.builder(
-                            itemCount: state.months.length,
-                            itemBuilder: (context, index) => Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        dateFormat.format(DateFormat("MM-yyyy")
-                                            .parse(state.months[index])),
-                                        style: AppTheme.lightBodyText.copyWith(
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                    ),
-                                    Column(
-                                      children: state.transactions
-                                          .where((transaction) =>
-                                              dateFormat.format(DateTime.parse(
-                                                  transaction.date
-                                                      .toString())) ==
-                                              dateFormat.format(
-                                                  DateFormat("MM-yyyy").parse(
-                                                      state.months[index])))
-                                          .map((transaction) =>
-                                              CategoryDetailsContainer(
-                                                transaction: transaction,
-                                              ))
-                                          .toList(),
+                          itemCount: state.months.length,
+                          itemBuilder: (context, index) => Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  dateFormat.format(
+                                    DateFormat(
+                                      "MM-yyyy",
+                                    ).parse(state.months[index]),
+                                  ),
+                                  style: AppTheme.lightBodyText.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                children: state.transactions
+                                    .where(
+                                      (transaction) =>
+                                          dateFormat.format(
+                                            DateTime.parse(
+                                              transaction.date.toString(),
+                                            ),
+                                          ) ==
+                                          dateFormat.format(
+                                            DateFormat(
+                                              "MM-yyyy",
+                                            ).parse(state.months[index]),
+                                          ),
                                     )
-                                  ],
-                                ));
+                                    .map(
+                                      (transaction) => CategoryDetailsContainer(
+                                        transaction: transaction,
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            ],
+                          ),
+                        );
                       } else if (state is CategoryDetailsErrorState) {
                         return ErrorMessage(message: state.errorMessage);
                       }
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
+                      return const Center(child: CircularProgressIndicator());
                     },
                   ),
                 ),
@@ -165,67 +170,115 @@ class CustomCategoryDetailsPage extends StatelessWidget {
                       InkWell(
                         onTap: () {
                           AppRouter.router
-                              .push(PAGES.insert.screenPath, extra: [
-                            Strings.add,
-                            TransactionModel(
-                                title: "",
-                                id: 0,
-                                category: category,
-                                type: Strings.income,
-                                amount: 0,
-                                description: "",
-                                date: DateFormat('yyyy-MM-dd')
-                                    .format(DateTime.now())),
-                            PAGES.categoryDetails.screenPath
-                          ]);
+                              .push(
+                                PAGES.insert.screenPath,
+                                extra: [
+                                  Strings.add,
+                                  TransactionModel(
+                                    title: "",
+                                    id: 0,
+                                    category: category,
+                                    type: Strings.income,
+                                    amount: 0,
+                                    description: "",
+                                    date: DateFormat(
+                                      'yyyy-MM-dd',
+                                    ).format(DateTime.now()),
+                                  ),
+                                  PAGES.categoryDetails.screenPath,
+                                ],
+                              )
+                              .then((result) {
+                                if (context.mounted) {
+                                  if (result == true) {
+                                    context.read<HomePageBloc>().add(
+                                      const HomePageEvent.started(),
+                                    );
+                                    context.read<CategoryDetailsBloc>().add(
+                                      CategoryDetailsEvent.started(
+                                        category: category,
+                                      ),
+                                    );
+                                  }
+                                }
+                              });
                         },
                         child: Container(
                           margin: const EdgeInsets.only(top: 2, bottom: 2),
                           width: MediaQuery.of(context).size.width * 0.43,
                           decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.circular(30)),
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
                           child: Center(
-                              child: Text("Add Income",
-                                  style: AppTheme.lightBodyText
-                                      .copyWith(fontWeight: FontWeight.w500))),
+                            child: Text(
+                              "Add Income",
+                              style: AppTheme.lightBodyText.copyWith(
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                       InkWell(
                         onTap: () {
                           AppRouter.router
-                              .push(PAGES.insert.screenPath, extra: [
-                            Strings.add,
-                            TransactionModel(
-                                title: "",
-                                id: 0,
-                                category: category,
-                                type: Strings.expense,
-                                amount: 0,
-                                description: '',
-                                date: DateFormat('yyyy-MM-dd')
-                                    .format(DateTime.now())),
-                            PAGES.categoryDetails.screenPath
-                          ]);
+                              .push(
+                                PAGES.insert.screenPath,
+                                extra: [
+                                  Strings.add,
+                                  TransactionModel(
+                                    title: "",
+                                    id: 0,
+                                    category: category,
+                                    type: Strings.expense,
+                                    amount: 0,
+                                    description: '',
+                                    date: DateFormat(
+                                      'yyyy-MM-dd',
+                                    ).format(DateTime.now()),
+                                  ),
+                                  PAGES.categoryDetails.screenPath,
+                                ],
+                              )
+                              .then((result) {
+                                if (context.mounted) {
+                                  if (result == true) {
+                                    context.read<HomePageBloc>().add(
+                                      const HomePageEvent.started(),
+                                    );
+                                    context.read<CategoryDetailsBloc>().add(
+                                      CategoryDetailsEvent.started(
+                                        category: category,
+                                      ),
+                                    );
+                                  }
+                                }
+                              });
                         },
                         child: Container(
                           margin: const EdgeInsets.only(top: 2, bottom: 2),
                           width: MediaQuery.of(context).size.width * 0.43,
                           decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.circular(30)),
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
                           child: Center(
-                              child: Text("Add Expense",
-                                  style: AppTheme.lightBodyText
-                                      .copyWith(fontWeight: FontWeight.w500))),
+                            child: Text(
+                              "Add Expense",
+                              style: AppTheme.lightBodyText.copyWith(
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
                         ),
-                      )
+                      ),
                     ],
                   ),
-                )
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
